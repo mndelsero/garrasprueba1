1d5ee3e5852fa5043a5da8ef63ad298e9cdd62d5
mndelsero <mndelsero@gmail.com>
2024-01-24 17:45:27 -0300

    imagenes

--- src/components/carousel/Carouselinicio.jsx
+++ src/components/carousel/Carouselinicio.jsx
@@ -6,19 +6,19 @@ const Carouselinicio = () => {
     <div>
       <Carousel className='carousel'>
       <Carousel.Item>
-       <img src="src\assets\evento viaje.jpeg" alt="" className='carousel-img'/>
+       <img src="/src/assets/evento viaje.jpeg" alt="" className='carousel-img'/>
         <Carousel.Caption>
           
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-       <img src="src\assets\evento-viaje.jpeg" alt="" className='carousel-img'/>
+       <img src="/src/assets/evento-viaje.jpeg" alt="" className='carousel-img'/>
         <Carousel.Caption>
           
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src="src\assets\foto graduado.jpeg" alt=""className='carouse-img' />
+        <img src="/src/assets/foto graduado.jpeg" alt=""className='carouse-img' />
         <Carousel.Caption>
          
         </Carousel.Caption>
